41a326163a5021776b6e0be4fff28146edee5579
nexdrew <andrew@npmjs.com>
2016-01-25 10:36:07 -0500

    drop subject from contact-support to avoid escaping

--- test/contact-support.js
+++ test/contact-support.js
@@ -4,12 +4,10 @@ import { loadTemplate } from './_utils'
 test('expansion of meta', async t => {
   let template = await loadTemplate('contact-support.meta.hbs')
   let metaString = template({
-    subject: 'Pick a subject. Any subject.',
     email: 'nunya@biznazz.yo',
     from: 'website@npmjs.com'
   })
   let meta = JSON.parse(metaString)
-  t.is(meta.subject, 'Pick a subject. Any subject.')
   t.is(meta.to, 'nunya@biznazz.yo')
   t.is(meta.from, '"npm, Inc. Support" <support@npmjs.com>')
 })
